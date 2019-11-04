c7392cc07cbb3397130a7c42203f8a233dfbc48c
alex <apakhomov@gridfore.com>
2019-11-04 22:20:29 +0300

    Add gensim example

--- run_docker_jupyter.sh
+++ run_docker_jupyter.sh
@@ -29,4 +29,8 @@ exec docker run --rm -u $(id -u):$(id -g) -v "$PWD:/notebooks" -w /notebooks -e 
 # which means that you can keep the changes you made, 
 # i.e. if you installed more software with pip.
 # 
-# USER_ID=$(id -u) GROUP_ID=$(id -g) GROUP_NAME=$(id -gn) exec docker-compose -f docker/docker-compose.yaml up
+#USER_ID=$(id -u) 
+#GROUP_ID=$(id -g) 
+#GROUP_NAME=$(id -gn) 
+
+#exec docker-compose -f docker/docker-compose.yaml up
